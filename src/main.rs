ca1d24aafb5ac2364bd30638e063df4d9a8d6322
julien-jungo <julienjungo+github.com@pm.me>
2026-01-02 23:34:54 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,253 @@
+use std::mem;
+
+enum Slot<T> {
+    Free(Option<usize>),
+    Used(T)
+}
+
+impl<T> Slot<T> {
+    fn as_free(&self) -> &Option<usize> {
+        match self {
+            Slot::Free(pos) => pos,
+            _ => panic!("expected free slot")
+        }
+    }
+
+    fn as_free_mut(&mut self) -> &mut Option<usize> {
+        match self {
+            Slot::Free(pos) => pos,
+            _ => panic!("expected free slot")
+        }
+    }
+
+    fn into_free(self) -> Option<usize> {
+        match self {
+            Slot::Free(pos) => pos,
+            _ => panic!("expected free slot")
+        }
+    }
+
+    fn as_used(&self) -> &T {
+        match self {
+            Slot::Used(val) => val,
+            _ => panic!("expected used slot")
+        }
+    }
+
+    fn as_used_mut(&mut self) -> &mut T {
+        match self {
+            Slot::Used(val) => val,
+            _ => panic!("expected used slot")
+        }
+    }
+
+    fn into_used(self) -> T {
+        match self {
+            Slot::Used(val) => val,
+            _ => panic!("expected used slot")
+        }
+    }
+}
+
+struct LinkedListNode<T> {
+    prev: Option<usize>,
+    next: Option<usize>,
+    val: T
+}
+
+struct LinkedList<T> {
+    size: usize,
+    head: Option<usize>,
+    tail: Option<usize>,
+    free: Option<usize>,
+    slots: Vec<Slot<LinkedListNode<T>>>
+}
+
+struct LinkedListIterator<'a, T> {
+    list: &'a LinkedList<T>,
+    curr: Option<usize>
+}
+
+impl<'a, T> LinkedListIterator<'a, T> {
+    fn new(list: &'a LinkedList<T>) -> LinkedListIterator<'a, T> {
+        LinkedListIterator { list, curr: list.head }
+    }
+}
+
+impl<'a, T> Iterator for LinkedListIterator<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let Some(pos) = self.curr else {
+            return None;
+        };
+
+        let node = self.list.slots[pos].as_used();
+
+        self.curr = node.next;
+
+        Some(&node.val)
+    }
+}
+
+impl<'a, T> IntoIterator for &'a LinkedList<T> {
+    type Item = &'a T;
+    type IntoIter = LinkedListIterator<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> LinkedList<T> {
+        LinkedList {
+            size: 0,
+            head: None,
+            tail: None,
+            free: None,
+            slots: Vec::new()
+        }
+    }
+
+    pub fn iter(&self) -> LinkedListIterator<'_, T> {
+        LinkedListIterator::new(self)
+    }
+
+    pub fn size(&self) -> usize {
+        return self.size
+    }
+
+    pub fn is_empty(&self) -> bool {
+        return self.head.is_none();
+    }
+
+    pub fn get_first(&self) -> Option<&T> {
+        self.head.map(|pos| &self.slots[pos].as_used().val)
+    }
+
+    pub fn get_last(&self) -> Option<&T> {
+        self.tail.map(|pos| &self.slots[pos].as_used().val)
+    }
+
+    pub fn add_first(&mut self, val: T) {
+        let node = LinkedListNode {
+            prev: None,
+            next: self.head,
+            val
+        };
+
+        let new_head = self.insert(node);
+
+        match self.head {
+            None => {
+                self.head = Some(new_head);
+                self.tail = Some(new_head);
+            },
+            Some(_) => {
+                self.head = Some(new_head);
+            }
+        }
+    }
+
+    pub fn add_last(&mut self, val: T) {
+        let node = LinkedListNode {
+            prev: self.tail,
+            next: None,
+            val
+        };
+
+        let new_tail = self.insert(node);
+
+        match self.tail {
+            None => {
+                self.head = Some(new_tail);
+                self.tail = Some(new_tail);
+            },
+            Some(old_tail) => {
+                self.slots[old_tail].as_used_mut().next = Some(new_tail)
+            }
+        }
+    }
+
+    pub fn remove_first(&mut self) -> Option<T> {
+        self.head.map(|pos| {
+            self.head = self.slots[pos].as_used().next;
+
+            if self.head.is_none() {
+                self.tail = None;
+            }
+
+            let slot = mem::replace(
+                &mut self.slots[pos],
+                Slot::Free(self.free)
+            );
+
+            self.free = Some(pos);
+            self.size -= 1;
+
+            slot.into_used().val
+        })
+    }
+
+    pub fn remove_last(&mut self) -> Option<T> {
+        self.tail.map(|pos| {
+            self.tail = self.slots[pos].as_used().prev;
+
+            if self.tail.is_none() {
+                self.head = None;
+            }
+
+            let slot = mem::replace(
+                &mut self.slots[pos],
+                Slot::Free(self.free)
+            );
+
+            self.free = Some(pos);
+            self.size -= 1;
+
+            slot.into_used().val
+        })
+    }
+
+    fn insert(&mut self, node: LinkedListNode<T>) -> usize {
+        let slot = Slot::Used(node);
+
+        self.size += 1;
+
+        match self.free {
+            None => {
+                self.slots.push(slot);
+                self.slots.len() - 1
+            },
+            Some(curr) => {
+                self.free = *self.slots[curr].as_free();
+                self.slots[curr] = slot;
+                curr
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut list: LinkedList<&str> = LinkedList::new();
+
+    println!("{}", list.is_empty());
+    println!("{}", list.size());
+
+    list.add_last("Hello");
+    list.add_last("World");
+
+    println!("{}", list.is_empty());
+    println!("{}", list.size());
+
+    for item in &list {
+        println!("{item}");
+    }
+
+    assert_eq!("Hello", list.remove_first().unwrap());
+    assert_eq!("World", list.remove_first().unwrap());
+    assert_eq!(None, list.remove_first());
+    assert_eq!(true, list.is_empty());
+    assert_eq!(0, list.size());
+}
